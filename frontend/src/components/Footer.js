9f7a07455a5cc0fd999e6ed920500548dd720114
Bernard Towindo <bernardtowindo38@gmail.com>
2024-05-21 08:53:04 +0200

    Frontend Commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200'>
+      <div className='container mx-auto p-4'>
+       <p className='text-center font-semibold' title="Developer">Dynamic Coding by Bernard Towindo</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
